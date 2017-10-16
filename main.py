48298166a7b4a47d23e09af4929917ef98a8bf56
Javier Rodriguez <frodriguez@intellego.com.mx>
2017-10-16 18:36:51 -0500

    First commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""Mapa de numeros decimales y romanos 1 to 4999"""
+"""Mapa de numeros decimales y romanos 1 to 3999"""
 
 ROMAN_MAP = {1:"I", 4:"IV", 5:"V", 9: "IX", 10:"X", 40:"XL", 50:"L",
              90:"XC", 100:"C", 400:"CD", 500:"D", 900:"CM", 1000:"M"}
